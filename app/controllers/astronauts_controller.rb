5c9435d9ff296ff1722b216abfbbc7e7bc5e7d06
Jon Patterson <JonPatt92@gmail.com>
2019-10-11 10:00:42 -0600

    Build feature test & add average age calculation to view for story 2

--- app/controllers/astronauts_controller.rb
+++ app/controllers/astronauts_controller.rb
@@ -2,4 +2,8 @@ class AstronautsController < ApplicationController
   def index
     @astronauts = Astronaut.all
   end
+
+  # def self.average_age
+  #   Astronaut.average(:age)
+  # end
 end
--- spec/features/astronaut/index_spec.rb
+++ spec/features/astronaut/index_spec.rb
@@ -7,19 +7,29 @@
 require 'rails_helper'
 
 describe 'When I visit /astronauts' do
-  it 'I see a list of astronauts with the following info:' do
-    @neil = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+  before(:each) do
+    @neil = Astronaut.create(name: "Neil Armstrong", age: 30, job: "Commander")
     @buzz = Astronaut.create(name: "Buzz Aldrin", age: 40, job: "Pilot")
+  end
+
+  it 'I see a list of astronauts with the following info:' do
 
     visit '/astronauts'
-    save_and_open_page
+
     expect(page).to have_content("Neil Armstrong")
     expect(page).to have_content("Buzz Aldrin")
     expect(page).to have_content("Commander")
     expect(page).to have_content("Pilot")
-    expect(page).to have_content(37)
+    expect(page).to have_content(30)
     expect(page).to have_content(40)
 
+  end
 
+  it 'On /astronauts I see average age of all astronauts' do
+    visit '/astronauts'
+    within "#avg-age" do
+      expect(page).to have_content("Average age of astronauts:")
+      expect(page).to have_content(35)
+    end
   end
 end
