9f2176ed921d44a1fa1cf5b80e7120675cee314a
Jon Patterson <JonPatt92@gmail.com>
2019-10-11 09:40:17 -0600

    Add feature test for astronauts index

--- spec/features/astronaut/index_spec.rb
+++ spec/features/astronaut/index_spec.rb
@@ -8,9 +8,18 @@ require 'rails_helper'
 
 describe 'When I visit /astronauts' do
   it 'I see a list of astronauts with the following info:' do
+    neil = Astronaut.new(Name: "Neil Armstrong", Age: 37, Job: "Commander")
+    buzz = Astronaut.new(Name: "Buzz Aldrin", Age: 40, Job: "Pilot")
 
     visit '/astronauts'
 
-    expect(page).to have_content("")
+    expect(page).to have_content("Neil Armstrong")
+    expect(page).to have_content("Buzz Aldrin")
+    expect(page).to have_content("Commander")
+    expect(page).to have_content("Pilot")
+    expect(page).to have_content(37)
+    expect(page).to have_content(40)
+    
+
   end
 end
